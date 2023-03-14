1829f06e714338fac91d5ae5e5dd81b1fbcfcce2
Ynneh <007trey2k@gmail.com>
2023-03-14 20:28:51 +0000

    - Added description to npcs for 210 spawn dump

--- ElvargServer/src/main/java/com/elvarg/game/entity/impl/npc/NPC.java
+++ ElvargServer/src/main/java/com/elvarg/game/entity/impl/npc/NPC.java
@@ -475,4 +475,9 @@ public class NPC extends Mobile {
 
 				}).collect(Collectors.toMap(ImmutablePair::getLeft, ImmutablePair::getRight));
 	}
+
+	public String description;
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
