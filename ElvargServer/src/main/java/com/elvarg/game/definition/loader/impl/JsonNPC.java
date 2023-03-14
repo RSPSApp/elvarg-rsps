bfe45fe7ded7d3bbb35dc7ec43c090e6454ed2b3
Ynneh <007trey2k@gmail.com>
2023-03-14 20:28:36 +0000

    - Added class format for the 210 spawn dump

---
+++ ElvargServer/src/main/java/com/elvarg/game/definition/loader/impl/JsonNPC.java
@@ -0,0 +1,49 @@
+package com.elvarg.game.definition.loader.impl;
+
+import com.elvarg.game.model.Direction;
+import com.elvarg.game.model.Location;
+
+/**
+ * @author Ynneh | 13/03/2023 - 16:38
+ * <https://github.com/drhenny>
+ */
+public class JsonNPC {
+
+        public String facing;
+        public int id;
+        public Location[] position;
+
+        public String description;
+
+        public JsonNPC(String facing, int id, Location[] position, String description) {
+            this.facing = facing;
+            this.id = id;
+            this.position = position;
+            this.description = description;
+        }
+
+        public Direction dir() {
+            if (facing != null) {
+                switch (facing) {
+                    case "NORTH":
+                        return Direction.NORTH;
+                    case "NORTH_EAST":
+                        return Direction.NORTH_EAST;
+                    case "NORTH_WEST":
+                        return Direction.NORTH_WEST;
+                    case "SOUTH":
+                        return Direction.SOUTH;
+                    case "SOUTH_EAST":
+                        return Direction.SOUTH_EAST;
+                    case "SOUTH_WEST":
+                        return Direction.SOUTH_WEST;
+                    case "EAST":
+                        return Direction.EAST;
+                    case "WEST":
+                        return Direction.WEST;
+                }
+            }
+            return Direction.SOUTH;
+        }
+
+}
