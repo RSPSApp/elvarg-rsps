76d373382c41c04391149fc3dbb5f03e49ce277b
Ynneh <007trey2k@gmail.com>
2023-08-31 14:23:47 +0100

    lightofsight check

--- ElvargServer/src/main/java/com/elvarg/game/collision/RegionManager.java
+++ ElvargServer/src/main/java/com/elvarg/game/collision/RegionManager.java
@@ -848,6 +848,10 @@ public class RegionManager {
         return canMove(position.getX(), position.getY(), end.getX(), end.getY(), position.getZ(), size, size, privateArea);
     }
 
+    public static boolean canMove(Location start, Location end, int xLength, int yLength) {
+        return canMove(start.getX(), start.getY(), end.getX(), end.getY(), start.getZ(), xLength, yLength, null);
+    }
+
     /**
      * Attemps to load the map files related to this region...
      */
@@ -951,5 +955,9 @@ public class RegionManager {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static boolean hasLineOfSight(Location a, Location b, int xLength, int yLength) {
+        return canProjectileMove(a.getX(), a.getY(), b.getX(), b.getY(), a.getZ(), xLength, yLength, null);
     }
 }
